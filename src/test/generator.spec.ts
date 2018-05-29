b1d964de69089558df2b0a570c2a0a96950cf8bf
Kermad <gkermad.ramzy@gmail.com>
2018-05-29 17:09:23 +0200

    #

--- src/test/generator.spec.ts
+++ src/test/generator.spec.ts
@@ -1,4 +1,5 @@
 // tslint:disable:only-arrow-functions
+// tslint:disable:no-console
 
 import * as dm from '../lib/index';
 import 'mocha';
@@ -9,22 +10,41 @@ import * as fs from 'fs';
 
 import * as request from 'request';
 
+const exists = promisify(fs.exists);
+const asyncMkDir = promisify(fs.mkdir);
+const readdir = promisify(fs.readdir);
+const unlink = promisify(fs.unlink);
+
+async function deleteDirectoryContent(dirPath: string) {
+    try {
+        const files = await readdir(dirPath);
+        const unlinkPromises = files.map((filename) => unlink(`${dirPath}/${filename}`));
+        await Promise.all(unlinkPromises);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 describe('Generator', function () {
     it('simple output', async function () {
         const tmpFolder = path.join(__dirname, '../../src/test/', 'tmp');
         // TODO creer le repertoire (et le vider)
+        if (await !exists(tmpFolder)) {
+            await asyncMkDir(tmpFolder);
+        } else {
+            await deleteDirectoryContent(tmpFolder);
+        }
 
         // TODO file:// + chemin vers model.docx;
-        const modelUrl = 'file://C:/Projects/Phenix/doc-merge/src/test/docx-generator-data/model.docx';
+        const modelUrl = 'file:\\\\' +  path.join(__dirname, '..\\..\\src\\test\\docx-generator-data', 'model.docx');
+        console.log(modelUrl);
         const app = new dm.App({
             port: 0,
             tmpFolder,
         });
-
         const server = await app.start();
         try {
             const port = server.address().port;
-
             const body: dm.IBody = {
                 data: {},
                 modeleRef: { url: modelUrl },
@@ -33,8 +53,6 @@ describe('Generator', function () {
             };
 
             const testUrl = `http://localhost:${port}/merge`;
-
-
             const requestResponse = await new Promise<request.Response>((resolve, reject) => {
                 request.post(testUrl,
                     {
